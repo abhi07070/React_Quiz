5396cf4df9e8970283ff6bc7c89888bb85aa7975
Davinder Kumar <abhiverma97819@gmail.com>
2023-11-09 13:09:23 +0530

    first commit

---
+++ src/components/Progress.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Progress = ({
+  index,
+  numOfQuestions,
+  points,
+  maxPossiblePoints,
+  answer,
+}) => {
+  return (
+    <header className="progress">
+      <progress max={numOfQuestions} value={index + Number(answer !== null)} />
+
+      <p>
+        Question <strong>{index + 1}</strong> / {numOfQuestions}
+      </p>
+      <p>
+        <strong>{points}</strong> / {maxPossiblePoints}
+      </p>
+    </header>
+  );
+};
+
+export default Progress;
