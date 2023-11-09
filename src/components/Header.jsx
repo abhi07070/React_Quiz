7e2921f030a8e844d3efb83528103eb301d22aab
Davinder Kumar <abhiverma97819@gmail.com>
2023-11-09 13:31:55 +0530

    adds footer

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,6 +5,13 @@ const Header = () => {
     <div className="app-header">
       {/* <img src="logo512.png" alt="React logo" /> */}
       <h1>The React Quiz</h1>
+      <p>
+        Developed By{" "}
+        <a href="https://github.com/abhi07070" target="_blank">
+          Davinder Kumar
+        </a>{" "}
+        💻
+      </p>
     </div>
   );
 };
